f8b9a272d2631fd30feb94aee1ca21f483cc4978
Fredy Hernández <fhernandez-2023176@kinal.edu.gt>
2025-05-11 21:51:15 -0600

    modificado: js por jsx en los hooks

---
+++ src/hooks/useComments.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+import { toast } from 'react-hot-toast';
+import { addComment, getCommentsByPublication } from '../services/api';
+
+export const useComments = (postId) => {
+    const [ comments, setComments ] = useState([]);
+    const [ loading, setLoading ] = useState(true);
+
+    const commentsFetch = async () => {
+        setLoading(true);
+        const response = await getCommentsByPublication(postId);
+        if(!response.error) setComments(response.data.comments || []);
+        setLoading(false)
+
+    }
+
+    const commentsSubmit = async ({ name, description }) => {
+        if(!name.trim() || !description.trim()) {
+            toast.error('Por favor completa todos los campos');
+            return false
+        }
+
+        const response = await addComment({ name, description, postId });
+        if(!response.error) {
+            toast.success('Comentario agregado');
+            setComments([]);
+            return true
+        }else {
+            toast.error('Error al agregar el comentario');
+            return false
+        }
+    }
+
+    useEffect(() => {
+        commentsFetch();
+    }, /*[postId]*/);
+
+    return {
+        comments,
+        loading,
+        commentsFetch,
+        commentsSubmit
+    }
+}
